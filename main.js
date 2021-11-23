5048f44487d6e276fdb42d023c0706565d00e9a1
Giorgio <1999giorgio.monne@mail.com>
2021-11-23 01:26:17 -1000

    da finire

--- main.js
+++ main.js
@@ -45,13 +45,68 @@ const posts = [
     },
     {
         "id": 5,
+
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+
         "media": "https://unsplash.it/600/400?image=534",
+
         "author": {
+
             "name": "Alessandro Sainato",
             "image": "https://unsplash.it/300/300?image=29"
         },
+
         "likes": 95,
+
         "created": "2021-03-05"
     }
 ];
+
+
+
+
+let card = "";
+
+for(let i=0; i <posts.length;i++){
+    card += `
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[i].author.name}</div>
+                <div class="post-meta__time">${posts[i].created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${posts[i].content}</div>
+        <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+
+                <div class="likes js-likes">
+
+                    <div class="likes__cta">
+
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+
+                    </div>
+
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+
+                </div> 
+
+            </div>
+    `; 
+}
+
+document.querySelector('.post').innerHTML = card;
+
+
